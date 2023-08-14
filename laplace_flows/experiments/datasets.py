2e76491ea720f70dd099d1959c783acacafea13d
Faried Abu Zaid <fariedaz@gmail.com>
2023-08-14 04:09:46 +0200

    Bugfix in Fashion dataset, add subsets by label

--- laplace_flows/experiments/datasets.py
+++ laplace_flows/experiments/datasets.py
@@ -10,6 +10,8 @@ import typing as T
 
 import idx2numpy
 
+from sklearn.datasets import make_moons, make_blobs, make_checkerboard, make_circles
+
 class DequantizedDataset(torch.utils.data.Dataset):
     """
     A dataset that dequantizes the data by adding uniform noise to each pixel.
@@ -36,54 +38,6 @@ class DequantizedDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.dataset)
     
-class FlattenedDataset(torch.utils.data.Dataset):
-    """
-    A dataset that flattens the data.
-    """
-    def __init__(self, dataset: torch.utils.data.Dataset):
-        self.dataset = dataset
-
-    def __getitem__(self, index: int):
-        x, y = self.dataset[index]
-        x = x.flatten()
-        return x, y
-
-    def __len__(self):
-        return len(self.dataset)
-    
-class FashionMnistDequantized(DequantizedDataset):
-    def __init__(self, dataloc: os.PathLike = None, train: bool = True):
-        rel_path = "FashionMNIST/raw/train-images-idx3-ubyte" if train else "FashionMNIST/raw/t10k-images-idx3-ubyte"
-        path = os.path.join(dataloc, rel_path)
-        if not os.path.exists(path):
-            FashionMNIST(path, train=train, download=True)
-        dataset = idx2numpy.convert_from_file(path)
-        dataset = dataset.reshape(dataset.shape[0], 1, 28, 28)     
-        super().__init__(dataset, num_bits=8)
-    
-    def __getitem__(self, index: int):
-
-        x = Tensor(self.dataset[index].copy())
-        x = self.transform(x)
-        return x, 0
-
-class MnistDequantized(DequantizedDataset):
-    def __init__(self, dataloc: os.PathLike = None, train: bool = True):
-        rel_path = "MNIST/raw/train-images-idx3-ubyte" if train else "MNIST/raw/t10k-images-idx3-ubyte"
-        path = os.path.join(dataloc, rel_path)
-        if not os.path.exists(path):
-            MNIST(path, train=train, download=True)
-        # TODO: remove hardcoding of 3x3 downsampling
-        dataset = idx2numpy.convert_from_file(path)[:, ::3, ::3]
-        dataset = dataset.reshape(dataset.shape[0], -1)     
-        super().__init__(dataset, num_bits=8)
-    
-    def __getitem__(self, index: int):
-
-        x = Tensor(self.dataset[index].copy())
-        x = self.transform(x)
-        return x, 0
-
 class DataSplit:
     def __init__(*agrs, **kwargs):
         raise NotImplementedError
@@ -100,6 +54,162 @@ class DataSplit:
     def get_val(self) -> torch.utils.data.Dataset:
         raise NotImplementedError
     
+class SimpleSplit(DataSplit):
+    """ 
+    Split of dataset
+    """
+    def __init__(
+        self,
+        train: torch.utils.data.Dataset,
+        test: torch.utils.data.Dataset,
+        val: torch.utils.data.Dataset
+    ):
+        """ Create split of dataset
+        
+        Args:
+            train (torch.utils.data.Dataset): training set
+            test (torch.utils.data.Dataset): test set
+            val (torch.utils.data.Dataset): validation set    
+        """
+        self.train = train
+        self.test = test
+        self.val = val
+    
+    def get_train(self) -> torch.utils.data.Dataset:
+        return self.train
+    
+    def get_test(self) -> torch.utils.data.Dataset:
+        return self.test
+    
+    def get_val(self) -> torch.utils.data.Dataset:
+        return self.val
+    
+ 
+ 
+GENERATORS = {
+    "make_moons": make_moons,
+    "make_blobs": make_blobs,
+    "make_checkerboard": make_checkerboard, 
+    "make_circles": make_circles
+}
+
+class SyntheticDataset(torch.utils.data.Dataset):
+    """
+    Dataset from generator function
+    """   
+    def __init__(
+        self,
+        generator: T.Union[T.Callable[..., np.ndarray], str],
+        params: T.Dict[str, T.Any],
+        *args,
+        **kwargs
+    ):
+        """Create dataset from generator function
+        
+        Args:
+            generator (function): generator function
+            params: ]dict]: parameters for generator function
+        """
+        super().__init__(*args, **kwargs)
+        if isinstance(generator, str):
+            generator = GENERATORS[generator]
+            
+        self.dataset = generator(**params)[0]
+    
+    def __getitem__(self, index: int):
+        x = self.dataset[index]
+        x = Tensor(x)
+        return x, torch.zeros_like(x)
+    
+    def __len__(self):
+        return len(self.dataset)
+
+class SyntheticSplit(SimpleSplit):
+    """ 
+    Split of synthetic dataset
+    """
+    def __init__(
+        self,
+        generator: T.Union[T.Callable[..., np.ndarray], str],
+        params_train: T.Dict[str, T.Any],
+        params_test: T.Dict[str, T.Any],
+        params_val: T.Dict[str, T.Any],
+        *args,
+        **kwargs
+    ):
+        """Create dataset from generator function
+        
+        Args:
+            generator (function): generator function
+            params: ]dict]: parameters for generator function
+        """
+        if isinstance(generator, str):
+            generator = GENERATORS[generator]
+            
+        train = SyntheticDataset(generator, params_train)
+        test = SyntheticDataset(generator, params_test)
+        val = SyntheticDataset(generator, params_val)
+        super().__init__(train=train, test=test, val=val, *args, **kwargs)
+     
+class FlattenedDataset(torch.utils.data.Dataset):
+    """
+    A dataset that flattens the data.
+    """
+    def __init__(self, dataset: torch.utils.data.Dataset):
+        self.dataset = dataset
+
+    def __getitem__(self, index: int):
+        x, y = self.dataset[index]
+        x = x.flatten()
+        return x, y
+
+    def __len__(self):
+        return len(self.dataset)
+    
+class FashionMnistDequantized(DequantizedDataset):
+    def __init__(self, dataloc: os.PathLike = None, train: bool = True, label: T.Optional[int] = None):
+        rel_path = "FashionMNIST/raw/train-images-idx3-ubyte" if train else "FashionMNIST/raw/t10k-images-idx3-ubyte"
+        path = os.path.join(dataloc, rel_path)
+        if not os.path.exists(path):
+            FashionMNIST(path, train=train, download=True)
+        # TODO: remove hardcoding of 3x3 downsampling
+        dataset = idx2numpy.convert_from_file(path)[:, ::3, ::3]
+        dataset = idx2numpy.convert_from_file(path)
+        dataset = dataset.reshape(dataset.shape[0], 1, 28, 28)  
+        if label is not None:
+            rel_path = "FashionMNIST/raw/train-labels-idx1-ubyte" if train else "FashionMNIST/raw/t10k-labels-idx1-ubyte"
+            path = os.path.join(dataloc, rel_path)
+            labels = idx2numpy.convert_from_file(path)
+            dataset = dataset[labels == label]     
+        super().__init__(dataset, num_bits=8)
+    
+    def __getitem__(self, index: int):
+
+        x = Tensor(self.dataset[index].copy())
+        x = self.transform(x)
+        return x, 0
+
+class MnistDequantized(DequantizedDataset):
+    def __init__(self, dataloc: os.PathLike = None, train: bool = True, digit: T.Optional[int] = None):
+        rel_path = "MNIST/raw/train-images-idx3-ubyte" if train else "MNIST/raw/t10k-images-idx3-ubyte"
+        path = os.path.join(dataloc, rel_path)
+        if not os.path.exists(path):
+            MNIST(path, train=train, download=True)
+        # TODO: remove hardcoding of 3x3 downsampling
+        dataset = idx2numpy.convert_from_file(path)[:, ::3, ::3]
+        dataset = dataset.reshape(dataset.shape[0], -1)   
+        if digit is not None:
+            rel_path = "MNIST/raw/train-labels-idx1-ubyte" if train else "MNIST/raw/t10k-labels-idx1-ubyte"
+            path = os.path.join(dataloc, rel_path)
+            labels = idx2numpy.convert_from_file(path)
+            dataset = dataset[labels == digit]  
+        super().__init__(dataset, num_bits=8)
+    
+    def __getitem__(self, index: int):
+
+        x = Tensor(self.dataset[index].copy())
+        x = self.transform(x)
+        return x, 0
 
 class DataSplitFromCSV(DataSplit):
     def __init__(self, train: os.PathLike, test: os.PathLike, val: os.PathLike):
@@ -117,15 +227,15 @@ class DataSplitFromCSV(DataSplit):
         return pd.read_csv(self.val).values
     
 class FashionMnistSplit(DataSplit):
-    def __init__(self, dataloc: os.PathLike = None, val_split: float = .1):
+    def __init__(self, dataloc: os.PathLike = None, val_split: float = .1, label: T.Optional[int] = None):
         if dataloc is None:
             dataloc = os.path.join(os.getcwd(), "data")
         self.dataloc = dataloc
-        self.train = FashionMnistDequantized(self.dataloc, train=True)
+        self.train = FashionMnistDequantized(self.dataloc, train=True, label=label)
         shuffle = torch.randperm(len(self.train))
         self.val = torch.utils.data.Subset(self.train, shuffle[:int(len(self.train) * val_split)])
         self.train = torch.utils.data.Subset(self.train, shuffle[int(len(self.train) * val_split):])
-        self.test = FashionMnistDequantized(self.dataloc, train=False)
+        self.test = FashionMnistDequantized(self.dataloc, train=False, label=label)
     
     def get_train(self) -> torch.utils.data.Dataset:
         return self.train
@@ -137,15 +247,15 @@ class FashionMnistSplit(DataSplit):
         return self.val
     
 class MnistSplit(DataSplit):
-    def __init__(self, dataloc: os.PathLike = None, val_split: float = .1):
+    def __init__(self, dataloc: os.PathLike = None, val_split: float = .1, digit: T.Optional[int] = None):
         if dataloc is None:
             dataloc = os.path.join(os.getcwd(), "data")
         self.dataloc = dataloc
-        self.train = MnistDequantized(self.dataloc, train=True)
+        self.train = MnistDequantized(self.dataloc, train=True, digit=digit)
         shuffle = torch.randperm(len(self.train))
         self.val = torch.utils.data.Subset(self.train, shuffle[:int(len(self.train) * val_split)])
         self.train = torch.utils.data.Subset(self.train, shuffle[int(len(self.train) * val_split):])
-        self.test = MnistDequantized(self.dataloc, train=False)
+        self.test = MnistDequantized(self.dataloc, train=False, digit=digit)
     
     def get_train(self) -> torch.utils.data.Dataset:
         return self.train
