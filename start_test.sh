aae236a11faa3256f249d3d0a60bfd131126886f
Faried Abu Zaid <fariedaz@gmail.com>
2024-01-12 13:15:41 +0100

    bugfix

--- start_test.sh
+++ start_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start TensorBoard in the background
-poetry run tensorboard --logdir=ray_results --bind_all &
 mkdir ray_results
 mkdir reports
+poetry run tensorboard --logdir=ray_results --bind_all &
 RAY=$(realpath ./ray_results)
 poetry run python scripts/run-experiment.py --config experiments/mnist/mnist.yaml --report_dir reports --storage_path $RAY
