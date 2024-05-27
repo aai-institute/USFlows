49b8e6d1b225d8d844e3b85a8682e8a76e9f3228
Giorgia Pitteri <g.pitteri@appliedai.de>
2024-05-27 13:18:47 +0200

    Sampling of norm distribution doesn't requiring unsqueezing operation if device is cpu.

--- src/veriflow/distributions.py
+++ src/veriflow/distributions.py
@@ -188,7 +188,10 @@ class RadialDistribution(torch.distributions.Distribution):
             sample_shape = tuple(sample_shape)
         
 
-        r = self.norm_distribution.sample(sample_shape).unsqueeze(-1).to(self.device)
+        if self.device == "cpu":
+            r = self.norm_distribution.sample(sample_shape).to(self.device)
+        else:
+            r = self.norm_distribution.sample(sample_shape).unsqueeze(-1).to(self.device)
 
         r = r.repeat(*[1 for _ in sample_shape], self.dim)
         u = self.unit_ball_distribution.sample(sample_shape).to(self.device)
