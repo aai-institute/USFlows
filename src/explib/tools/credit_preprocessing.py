a36eec97bbeb27205967d8c614ef2b1638c5c9e5
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-06-27 11:11:34 +0200

    Don't do standadrization on the dataset after min-max transform and make sure we can actually reconstruct the data by inverting the preprocessing step.

--- src/explib/tools/credit_preprocessing.py
+++ src/explib/tools/credit_preprocessing.py
@@ -18,13 +18,14 @@ def preprocess_data(input_file, output_file):
     if "Id" in data.columns:
         data = data.drop(["Id"], axis=1)
 
+
+    # One-hot encode specified features
+    one_hot_features = ['verw', 'famges', 'wohn']
+    data = pd.get_dummies(data, columns=one_hot_features)
+
     # Scaling to range [0, 1]
     min_max_scaler = MinMaxScaler()
     data_scaled = min_max_scaler.fit_transform(data)
-
-    # Standardizing the data
-    #standard_scaler = StandardScaler()
-    #data_standardized = standard_scaler.fit_transform(data_scaled)
 
     # Convert the standardized data back to DataFrame
     data_standardized_df = pd.DataFrame(data_scaled, columns=data.columns)
@@ -35,42 +36,46 @@ def preprocess_data(input_file, output_file):
     # Save the scalers for inverse transform later
     with open('min_max_scaler.pkl', 'wb') as f:
         pickle.dump(min_max_scaler, f)
-    #with open('standard_scaler.pkl', 'wb') as f:
-    #    pickle.dump(standard_scaler, f)
 
     print(f"Processed data saved to {output_file}")
 
-def inverse_transform(processed_data_file):
+def inverse_transform(processed_data_file, input_file):
     # Load the processed data
     data_processed = pd.read_csv(processed_data_file)
 
     # Load the scalers
     with open('min_max_scaler.pkl', 'rb') as f:
         min_max_scaler = pickle.load(f)
-    #with open('standard_scaler.pkl', 'rb') as f:
-    #    standard_scaler = pickle.load(f)
-
-    # Inverse transform the standardization
-    #data_unstandardized = standard_scaler.inverse_transform(data_processed)
 
     # Inverse transform the scaling to [0, 1]
     original_data = min_max_scaler.inverse_transform(data_processed)
-
+    original_data = original_data.round(0).astype(int)
     # Convert back to DataFrame
     original_data_df = pd.DataFrame(original_data, columns=data_processed.columns)
-    return original_data_df
+    encoded_vars = "verw_0,verw_1,verw_2,verw_3,verw_4,verw_5,verw_6,verw_8,verw_9,verw_10,famges_1,famges_2,famges_3,famges_4,wohn_1,wohn_2,wohn_3".split(",")
+    original_data_df_decode_dummies = pd.from_dummies(original_data_df[encoded_vars], sep="_")
+    original_data_df=original_data_df.drop(columns=encoded_vars)
+    merged_data = pd.concat([original_data_df, original_data_df_decode_dummies], axis=1)
+
+    data_processed = pd.read_csv(input_file)
+    data_reconstructed_with_id = pd.concat([merged_data, data_processed["Id"]], axis=1)
+    data_reconstructed_with_id = data_reconstructed_with_id["Id,laufkont,laufzeit,moral,verw,hoehe,sparkont,beszeit,rate,famges,buerge,wohnzeit,verm,alter,weitkred,wohn,bishkred,beruf,pers,telef,gastarb,kredit".split(",")]
+    return data_reconstructed_with_id
 
 
 # Example usage:
 # Preprocess data
 input_file = '/home/mustafa/repos/VeriFlow/experiments/credit/dataset/credit/train_positive.csv'
-output_file = '/home/mustafa/repos/VeriFlow/experiments/credit/dataset/credit/train_positive_processed.csv'
+output_file = '/home/mustafa/repos/VeriFlow/experiments/credit/dataset/credit/train_positive_processed_one_hot.csv'
 preprocess_data(input_file, output_file)
 
 # Inverse transform
 reconstructed_file = '/home/mustafa/repos/VeriFlow/experiments/credit/dataset/credit/recontructed.csv'
-original_data = inverse_transform(output_file)
+original_data = inverse_transform(output_file, input_file)
 # Save the processed data to CSV
 original_data.to_csv(reconstructed_file, index=False)
 print("Inverse transformed data:")
 print(original_data)
+
+
+# todo one-hot encoding of "verwendung", "famges", "wohn" ?,
