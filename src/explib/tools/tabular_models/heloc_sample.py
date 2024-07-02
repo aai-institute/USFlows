7d456f32da2dfe9fd251e4e1671a2f7cc0cdc7f3
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-07-02 13:54:01 +0200

    toggle standard scale for sampling.

--- src/explib/tools/tabular_models/heloc_sample.py
+++ src/explib/tools/tabular_models/heloc_sample.py
@@ -22,19 +22,27 @@ def samples_encoded_to_csv(samples, csv_path):
 
 
 if __name__ == '__main__':
-    model_path = '/scripts/reports/model_heloc_forward.onnx'
+
+    USE_SCALE = True
+
+    model_path = '/home/mustafa/repos/VeriFlow/experiments/credit/report/credit/archive/0_credit_1/2024-07-0121:55:42.385660/model_heloc_forward.onnx'
     ort_sess = ort.InferenceSession(model_path)
     # Load the scalers
-    with open('standard_scaler_positive.pkl', 'rb') as f:
-        standard_scaler = pickle.load(f)
+    if USE_SCALE:
+        with open('/home/mustafa/repos/VeriFlow/src/explib/tools/tabular_models/standard_scaler_positive.pkl', 'rb') as f:
+            standard_scaler = pickle.load(f)
 
     samples = []
     for i in range(100):
         x = np.random.uniform(low=-0.001, high=0.001, size=(22,)).astype(np.float32)
         outputs = ort_sess.run(None, {'x.1': x})
-        original_data = standard_scaler.inverse_transform(outputs[0].reshape(1, -1))
-        samples.append([i for i in original_data[0]])
-    raw_outputs_path = '/experiments/credit/dataset/heloc/samples.csv'
+        if USE_SCALE:
+            original_data = standard_scaler.inverse_transform(outputs[0].reshape(1, -1))
+            samples.append([round(i) for i in original_data[0]])
+        else:
+            samples.append([i for i in outputs[0]])
+
+    raw_outputs_path = '/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/samples.csv'
     samples_encoded_to_csv(samples, raw_outputs_path)
 
 
