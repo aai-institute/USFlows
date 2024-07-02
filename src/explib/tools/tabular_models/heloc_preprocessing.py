d0a281caeed4816b959195f468a6270e1be3650e
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-07-02 11:06:49 +0200

    Also save the unscaled subsets of the dataset.

--- src/explib/tools/tabular_models/heloc_preprocessing.py
+++ src/explib/tools/tabular_models/heloc_preprocessing.py
@@ -5,8 +5,12 @@ import pickle
 
 full_data_path = "/experiments/credit/dataset/heloc/heloc_full.csv"
 full_data_output = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_full_scaled.csv"
-positive_data_output = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_positive_scaled.csv"
-negative_data_output = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_negative_scaled.csv"
+
+positive_data_output_original = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_positive_originl.csv"
+positive_data_output_scaled = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_positive_scaled.csv"
+
+negative_data_output_original = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_negative_original.csv"
+negative_data_output_scaled = "/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_negative_scaled.csv"
 
 
 def scale_save_data(dataframe, scaler_name):
@@ -31,11 +35,17 @@ def preprocess_data():
 
     full_dataset = scale_save_data(data,'standard_scaler_full.pkl')
     full_dataset.to_csv(full_data_output, index=False)
-    positive_data = scale_save_data(data[data['RiskPerformance'] == 0.0], 'standard_scaler_positive.pkl')
-    positive_data.to_csv(positive_data_output, index=False)
-    negative_data = scale_save_data(data[data['RiskPerformance'] == 1.0], 'standard_scaler_negative.pkl')
-    negative_data.to_csv(negative_data_output, index=False)
-    print("asdf")
+
+    original_positive_data = data[data['RiskPerformance'] == 0.0]
+    original_positive_data.to_csv(positive_data_output_original, index=False)
+    scaled_positive_data = scale_save_data(original_positive_data, 'standard_scaler_positive.pkl')
+    scaled_positive_data.to_csv(positive_data_output_scaled, index=False)
+
+    original_negative_data = data[data['RiskPerformance'] == 1.0]
+    original_negative_data.to_csv(negative_data_output_original, index=False)
+    scaled_negative_data = scale_save_data(original_negative_data, 'standard_scaler_negative.pkl')
+    scaled_negative_data.to_csv(negative_data_output_scaled, index=False)
+
 
 if __name__ == '__main__':
     preprocess_data()
