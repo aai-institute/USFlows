32a9a6bd4665b493eb9b6173d4b381b0c1229613
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-07-02 13:46:11 +0200

    Add script for measuring the quality of the flow model via local outlier factor.

---
+++ src/explib/tools/tabular_models/check_quality.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Assuming heloc_data is your real dataset and generated_data is your synthetic dataset
+    # heloc_data = pd.read_csv('heloc_dataset.csv')
+    # generated_data = pd.read_csv('generated_dataset.csv')
+
+    heloc_data = pd.read_csv('/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/heloc_positive_original.csv')
+    generated_data = pd.read_csv('/home/mustafa/repos/VeriFlow/experiments/credit/dataset/heloc/samples.csv')
+    # Combine the datasets
+    #filtered_data =heloc_data.filter(items=["ExternalRiskEstimate","MSinceOldestTradeOpen"]).reset_index(drop=True)
+    generated_scores = []
+    lof_scores = []
+    for index, row in generated_data.iterrows():
+        combined_data = heloc_data._append(row)
+        #pd.concat([heloc_data,row], axis=0).reset_index(drop=True)
+
+        # Train the LOF model on the combined dataset
+        lof = LocalOutlierFactor(n_neighbors=20)  # You can adjust n_neighbors and contamination as needed
+        lof.fit(combined_data)
+
+        # Calculate LOF scores (negative_outlier_factor_)
+        lof_scores = lof.negative_outlier_factor_
+
+        generated_scores.append(lof_scores[len(lof_scores)-1])
+
+    # Separate the scores for real and generated data
+    real_scores = lof_scores[:len(heloc_data)]
+    # Analyze the results
+    print(f'Real data LOF scores mean: {np.mean(real_scores)}, std: {np.std(real_scores)}')
+    print(f'Generated data LOF scores mean: {np.mean(generated_scores)}, std: {np.std(generated_scores)}')
+
+    plt.hist(real_scores, bins=10, alpha=0.5, label='Real Data')
+    plt.legend(loc='upper right')
+    plt.xlabel('LOF Score')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of LOF Scores')
+    plt.show()
+
+    plt.hist(generated_scores, bins=10, alpha=0.5, label='Generated Data')
+    plt.legend(loc='upper right')
+    plt.xlabel('LOF Score')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of LOF Scores')
+    plt.show()
