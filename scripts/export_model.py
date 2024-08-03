20088f5546b69ca04cd0e0b54562fdb08379d1fc
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-08-03 17:16:52 +0200

    Note bugfix for export problems.

--- scripts/export_model.py
+++ scripts/export_model.py
@@ -3,7 +3,20 @@ from src.explib.config_parser import from_checkpoint
 
 if __name__ == '__main__':
     print("Exporting model")
-    model = from_checkpoint("./config.pkl","./config.pt")
+    model = from_checkpoint("./a.pkl","./a.pt")
     model = model.simplify()
     model.to_onnx(path="./forward.onnx", export_mode="forward")
     model.to_onnx(path="./backward.onnx", export_mode="backward")
+
+
+"""
+BUGFIX first: this script implicitly calls UntypedStorage::_load_from_bytes somehwere, which is missing the
+param map_location.
+
+torch/storage.py
+
+
+def _load_from_bytes(b):
+    return torch.load(io.BytesIO(b), map_location='cpu')
+
+"""
