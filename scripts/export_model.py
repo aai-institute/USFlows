4ca01bc6d86db23e041a3ce151b2e85ee5b48acd
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-08-05 10:16:24 +0200

    update export model script.

--- scripts/export_model.py
+++ scripts/export_model.py
@@ -3,10 +3,13 @@ from src.explib.config_parser import from_checkpoint
 
 if __name__ == '__main__':
     print("Exporting model")
-    model = from_checkpoint("./a.pkl","./a.pt")
+    PATH = "/home/mustafa/Documents/midas/flows_unscaled_mnist/_trial_81c50_00004_4_batch_size=128,coupling_layers=2,coupling_nn_layers=300,lr=0.0001_2024-08-03_18-21-58/"
+    pkl = "params.pkl"
+    pt = "checkpoint.pt"
+    model = from_checkpoint(PATH+pkl, PATH+pt)
     model = model.simplify()
-    model.to_onnx(path="./forward.onnx", export_mode="forward")
-    model.to_onnx(path="./backward.onnx", export_mode="backward")
+    model.to_onnx(path=PATH+"/forward.onnx", export_mode="forward")
+    model.to_onnx(path=PATH+"./backward.onnx", export_mode="backward")
 
 
 """
