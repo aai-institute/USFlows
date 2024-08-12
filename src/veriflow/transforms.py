4f58965ccc2c797cfbf30cabe85ad940cc52c773
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-08-12 09:49:09 +0200

    try to set tensor to cuda directly.

--- src/veriflow/transforms.py
+++ src/veriflow/transforms.py
@@ -94,7 +94,7 @@ class ScaleTransform(BaseTransform):
         Returns:
             torch.Tensor: transformed tensor $\mathbf{scale}^{-1}x$
         """
-        return x / self.scale
+        return x.to("cuda") / self.scale
 
     def _call(self, x: torch.Tensor) -> torch.Tensor:
         """ Alias for :func:`forward`"""
