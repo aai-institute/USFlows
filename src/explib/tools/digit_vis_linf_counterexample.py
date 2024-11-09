81ce644237c25ec64b10584e204e8681b8fe0e05
Mustafa Yalciner <mustafa.yalciner@tu-dortmund.de>
2024-11-09 16:55:20 +0100

    Write script that checks if the density peak of the flow is classified correctly (Allows for checking the runtime for verification with Marabou.

--- src/explib/tools/digit_vis_linf_counterexample.py
+++ src/explib/tools/digit_vis_linf_counterexample.py
@@ -25,7 +25,7 @@ def forward_random_vector(model):
     input_shape = model.get_inputs()[0].shape
     print(f'p={p} and quantile={quantile_log_normal(p)}')
     random_input = np.random.choice([-quantile_log_normal(p), quantile_log_normal(p)], size=input_shape).astype(np.float32)
-    #random_input = np.zeros(input_shape).astype(np.float32)
+
     # Perform inference
     output = model.run(None, {input_name: random_input})
     output_vector = output[0]
@@ -36,13 +36,34 @@ def forward_random_vector(model):
     return clipped_output
 
 
-def visualize_output(output_vector, reshape=RESHAPE):
+def visualize_flow_output_border(output_vector, reshape=RESHAPE):
     """Visualizes the output vector as an image."""
     output_image = output_vector.reshape(reshape)
     plt.imshow(output_image, cmap='gray')
     plt.axis('off')
-    plt.title("Model Output")
+    plt.title(f"UDL {p}")
     plt.show()
+
+def visualize_flow_output_center(output_vector, predicted_class, reshape=RESHAPE):
+    """Visualizes the output vector as an image."""
+    output_image = output_vector.reshape(reshape)
+    plt.imshow(output_image, cmap='gray')
+    plt.axis('off')
+    plt.title(f"Zero vector, class prediction {predicted_class}")
+    plt.show()
+
+def check_class_of_zero_vector(classifier, model):
+    input_name = model.get_inputs()[0].name
+    input_shape = model.get_inputs()[0].shape
+    random_input = np.zeros(input_shape).astype(np.float32)
+    # Perform inference
+    output = model.run(None, {input_name: random_input})
+    output_vector = output[0]
+    digit_predict = classifier.run(None, {model.get_inputs()[0].name: output_vector})[0].argmax()
+
+    # Apply clipping to the output vector (e.g., between 0 and 1)
+    clipped_output = np.clip(output_vector, 0, 1)
+    return digit_predict, clipped_output
 
 if __name__ == '__main__':
     # Example usage
@@ -58,7 +79,15 @@ if __name__ == '__main__':
         "/home/mustafa/Documents/midas/all_digits_lu_small/8_mnist_logNormal_linf_digit_2/forward.onnx",
         "/home/mustafa/Documents/midas/all_digits_lu_small/9_mnist_logNormal_linf_digit_1/forward.onnx",
     ]
+
+    SHOW_CENTER = True
+
+    classifier_path = "/home/mustafa/repos/VeriFlow/experiments/verification/resources/MnistClassifier_14_14.onnx"
     for onnx_path in onnx_paths:
-        model = load_model(onnx_path)
-        output_vector = forward_random_vector(model)
-        visualize_output(output_vector)
+        flow_model = load_model(onnx_path)
+        classifier = load_model(classifier_path)
+        output_vector = forward_random_vector(flow_model)
+        visualize_flow_output_border(output_vector)
+        if SHOW_CENTER:
+            predicted_digit, center_clipped = check_class_of_zero_vector(classifier, flow_model)
+            visualize_flow_output_center(center_clipped, predicted_digit)
