Bugfix Ray Hyperopt: Avoid "deepcopy messing computation graph"

src/explib/hyperopt.py
@@ -20,10 +20,11 @@ from ray import tune
 from ray.air import RunConfig, session
 
 from src.explib.base import Experiment
-from src.explib.config_parser import from_checkpoint
+from src.explib.config_parser import from_checkpoint, create_objects_from_classes
 from src.veriflow.flows import NiceFlow
 from src.veriflow.networks import AdditiveAffineNN
 from src.veriflow.transforms import ScaleTransform
+
 
 
 
@@ -78,6 +79,7 @@ class HyperoptExperiment(Experiment):
         Returns:
             Dict[str, float]: trial performance metrics
         """
+        config = create_objects_from_classes(config)
         writer = SummaryWriter()
         # warnings.simplefilter("error")
         torch.autograd.set_detect_anomaly(True)

src/veriflow/flows.py
@@ -283,7 +283,7 @@ class USFlow(Flow):
 
     def __init__(
         self, 
-        base_distribution,
+        base_distribution: dist.Distribution,
         in_dims: List[int], 
         coupling_blocks: int,
         conditioner_cls: Type[torch.nn.Module],